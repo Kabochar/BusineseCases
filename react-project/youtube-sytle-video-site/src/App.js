af6257edfce89e1a83f0ddbcc2bfccfffa321e2e
kabocha <anemailbox@163.com>
2023-07-28 00:15:20 +0800

    init project foundation

--- react-project/youtube-sytle-video-site/src/App.js
+++ react-project/youtube-sytle-video-site/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import { Box } from '@mui/material';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>HelloWorld</h1>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{
+      backgroundColor: "#000"
+    }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App;
+export default App
