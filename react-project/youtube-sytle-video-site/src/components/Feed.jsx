00f32d8fb76809c32a551906d8db1e1cf951a1e5
kabochar <anemailbox@163.com>
2023-07-28 21:08:54 +0800

    feat(react/youtube): init header navbar

--- react-project/youtube-sytle-video-site/src/components/Feed.jsx
+++ react-project/youtube-sytle-video-site/src/components/Feed.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Box, Stack, Typography } from "@mui/material";
 
-function Feed() {
+const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{
+      flexDirection: {
+        sx: "column",
+        md: "row"
+      }
+    }}>
+      <Box sx={{
+        height: {
+          sx: "auto",
+          md: "92vh"
+        },
+        borderRight: "1px solid #3d3d3d",
+        px: {sx: 0, md: 2}
+      }}>
+        <Typography className='copyright' variant='body2'>
+          Copyright © 2023 kabochar
+        </Typography>
+      </Box>
+      <Box>
+        
+      </Box>
+    </Stack>
   )
-}
+};
+
 
 export default Feed;
--- react-project/youtube-sytle-video-site/src/components/Navbar.jsx
+++ react-project/youtube-sytle-video-site/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import Logo from "../asserts/favicon.ico"
+import SearchBar from './SearchBar';
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+
+const Navbar = () => (
+  <Stack direction="row" alignContent="center" p={2} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      {/* <img src={Logo} alt="logo" height={45} /> */}
+    </Link>
+    <SearchBar />
+  </Stack>
+);
 
 export default Navbar;
